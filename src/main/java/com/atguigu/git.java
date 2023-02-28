5196763a42d1d549f130db7358205b3c2a40ce39
QiuShuai <QiuShuai@RoadingToCode.com>
2023-02-28 16:58:39 +0800

    third commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello,get");
         System.out.println("hello,get");
+        System.out.println("hello,get3");
     }
 }
